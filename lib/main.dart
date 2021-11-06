5066a6621410c155a9eadd3a2925a116ba52a24d
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-11-06 12:47:04 +0700

    Update Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,21 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(child: Text("Home")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.bottomSheet(
+              ListView(
+                children: const [
+                  Text("Hello"),
+                ],
+              ),
+              backgroundColor: Colors.white,
+            );
+          },
+          child: const Text("Bottom Sheet"),
+        ),
+      ),
     );
   }
 }
